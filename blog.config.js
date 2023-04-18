df4810fffb658588fa72846dac45ded567ad400d
jacujay <lovejacujay@gmail.com>
2023-04-18 17:26:05 +0800

    修改关键词和首页文字

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '崩', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一位空想理想主义者☄', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.icarusai.space/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '鸡汤与钥匙', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '技术分享, 博客, chatgpt, AI, 心得感悟', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'jacujay@qq.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
